bfbbe71822b8630807b6a1d43cf33a128ad1d710
frozen-coder <83887503+frozen-coder@users.noreply.github.com>
2024-02-18 15:29:23 -0800

    sdfjoislkjgfh

---
+++ FetchPrototype.js
@@ -0,0 +1,27 @@
+const OpenAI = require('openai');
+
+const openai = new OpenAI({
+    apiKey:"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",
+});
+
+const userInput = 'Success Tips';
+
+const getResponse = async () => {
+    const response = await openai.chat.completions.create ({
+        model: 'gpt-3.5-turbo',
+        messages: [
+            {
+                role: 'user',
+                content: 'Format my day into a schedual: 1. Math, 4hrs, 2. language arts, 2 hrs. I eat 3 meals a day, i go to bed a 9pm and get up at 7am. Give a binary file',
+            },
+        ],
+        temperature: 0,
+        max_tokens: 500,
+        top_p: 1.0,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+    });
+    console.log(response.choices[0].message);
+};
+
+getResponse();

61fcbb61864740c359ecac51d490f2a2684f29ca
frozen-coder <83887503+frozen-coder@users.noreply.github.com>
2024-02-18 18:20:42 -0800

    fixxed

--- FetchPrototype.js
+++ FetchPrototype.js
@@ -1,8 +1,7 @@
 const OpenAI = require('openai');
-require('dotenv').config()
 
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_KEY,
+    apiKey:"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",
 });
 
 
@@ -12,12 +11,12 @@ const getResponse = async () => {
         messages: [
             {
                 role: 'user',
-                content: 'Format my day into a schedual: 1. Math, 4hrs, 2. language arts, 2 hrs. I eat 3 meals a day, i go to bed a 9pm and get up at 7am. Give a binary file',
+                content: 'TasksToSchedual:Math,LA,breaks,3Meals(breakfast,lunch,dinner),BedAt9pmUpAt7am.ReturnOneLineWithJsonFormat(title:[StartTime,EndTime]).UseMilitaryTime.RemoveAllWhitespace',
             },
         ],
         temperature: 0,
         max_tokens: 500,
-        top_p: 1.0,
+        top_p: .1,
         frequency_penalty: 0.0,
         presence_penalty: 0.0,
     });

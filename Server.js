6ba57a648d0d0c2f7b17f2991603b22704a15b5d
frozen-coder <83887503+frozen-coder@users.noreply.github.com>
2024-07-20 13:44:15 -0700

    Update Server.js

--- Server.js
+++ Server.js
@@ -5,9 +5,10 @@ const app = express();
 var bodyParser = require('body-parser')
 let jsonParser = bodyParser.json()
 const OpenAI = require("openai");
-const openai = new OpenAI();
+const openai = new OpenAI({
+  apiKey: "sk-proj-xHOQgq2cciPMvX7Q5WnxT3BlbkFJVSwuM1ixpNrMnfUXq3gM"
+});
 //let API_KEY = "AIzaSyA32Swkb_ID3NkXoPziqikdh7--Y4yVdhs";
-const API_KEY="sk-proj-xHOQgq2cciPMvX7Q5WnxT3BlbkFJVSwuM1ixpNrMnfUXq3gM";
 const fs = require("fs");
 const { homedir } = require('os'); 
 var userInformation = {
@@ -28,7 +29,9 @@ async function runOrganize(activityNames) {
   const completion = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     input = "Format my day into a schedual with times as an array in JSON format with the following activities: " 
-      + names + " dont duplicate items. User Fitnesss Level = " + userInformation.FitnessLevel + ". EnjoyedActivities = " + Enj
+      + activityNames + " dont duplicate items. User Fitnesss Level = " + userInformation.FitnessLevel 
+      + ". EnjoyedActivities = " + userInformation.EnjoyedActivities + ". DislikedActivities "
+       + userInformation.DislikedActivities + ". Age: " + userInformation.;
     messages: [
       {
         role: "user",
@@ -41,21 +44,11 @@ async function runOrganize(activityNames) {
     frequency_penalty: 0.0,
     presence_penalty: 0.0,
   });
-
-  const prompt = "What is in this image in one word without a period?";
-
-  const imageParts = [
-    fileToGenerativePart("./sketch.png", "image/png"),
-  ];
-  
-
-  const result = await model.generateContent([prompt, ...imageParts]);
-  const response = await result.response;
-  const text = response.text();
-  console.log(text);
-  return text;
+  surveryresponse = JSON.parse(response.choices[0].message.content)["schedule"];
+  console.log(surveryresponse);
+  return surveryresponse;
 }
-
+/*
 async function Promptrun() {
   // For text-and-image input (multimodal), use the gemini-pro-vision model
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
@@ -74,10 +67,10 @@ async function Promptrun() {
   console.log(text);
   return text;
 }
-
+*/
 
 app.use(express.static(path.join(__dirname, 'Static')));
-
+/*
 app.use('/AI', (req, res, next) => {  // req: Request, Res: Result, next: to go to the next Middle ware 
   const secondFunction = async () => { // The Secondfunction is a wrapper functions what is a async function to is returns a promice, and it waits for the run fuction because the AI is not instant, and then sends it to the server. 
     const result = await run();
@@ -87,7 +80,15 @@ app.use('/AI', (req, res, next) => {  // req: Request, Res: Result, next: to go 
   }
   secondFunction();
 });
+*/
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'Static', 'LandingPage.html'));
+})
 
+app.get("/index", (req, res) => {
+  res.sendFile(path.join(__dirname, 'Static', 'LandingPage.html'));
+})
+/*
 app.get('/AI', (req, res) => {
   res.send();
 });
@@ -115,6 +116,17 @@ app.get('/index', (req, res) => {
 app.get('/wait', (req, res) => {
   res.sendFile(path.join(__dirname, 'Static', 'WaitingSinglePlayer.html'));
 });
+*/
+app.post("/post/survey", jsonParser, (req, res) => {
+  userInformation.FitnessLevel = req.FitnessLevel;
+  userInformation.DislikedActivities = req.DislikedActivities;
+  userInformation.Height = req.Height;
+  userInformation.Weight = req.Weight;
+  userInformation.Age = req.Age;
+  console.log(userInformation);
+
+});
+/*
 //post request
 app.post("/upload", jsonParser, (req, res) => {
   const imageData = req.body.imageData;
@@ -143,5 +155,6 @@ app.post('/prompt', jsonParser, (req, res) => {
 app.get('/prompt', (req, res) => {
   res.json({ prompt: currentPrompt });
 });
+*/
 
 app.listen(3000, () => console.log('Example app is listening on port 3000.'));
